1e4d2a8ac24ec5b245b3d348f4a2ca3237f0e610
Bartosz Gawin <bartex.gw@gmail.com>
2023-09-21 18:40:46 +0100

    started working on discontinousNN

--- models/MlpNetwork.py
+++ models/MlpNetwork.py
@@ -47,7 +47,7 @@ class DiscontinuityDense(nn.Module):
 
         # Discontinuity initialization
         if discontinuity_initializer is None:
-            nn.init.zeros_(self.discontinuity)
+            nn.init.xavier_uniform_(self.discontinuity)
         else:
             discontinuity_initializer(self.discontinuity)
 
@@ -70,3 +70,61 @@ class DiscontinuityDense(nn.Module):
         # L(x) = f(W'x + b) + eps * H(W'x + b)
         out = wx + eps_h_wx_b
         return out
+
+
+class DiscontinuousNeuralNetwork(nn.Module):
+    def __init__(self,
+                 activation,
+                 depth:int,
+                 input_dim:int,
+                 output_dim:int,
+                 n_units:int,
+                 d_units:int,
+                 embd_size = None,
+                 n_cat = None):
+        super(DiscontinuousNeuralNetwork, self).__init__()
+        assert (embd_size is None) or (embd_size is not None and isinstance(n_cat, int)), "n_cat must be an integer if embd_size is not None"
+        assert depth >= 3, "depth cannot be lower than 1"
+
+        self.activation = activation  # PyTorch activation function like nn.ReLU()
+
+
+
+        if embd_size:
+            self.embeds = nn.Embedding(n_cat, embd_size)
+        else:
+            self.embeds = None
+
+        if self.embeds is not None:
+            self.layers = nn.ModuleList([nn.Linear(input_dim - n_cat, n_units)])
+        else:
+            self.layers = nn.ModuleList([nn.Linear(input_dim, n_units)])
+
+        for i in range(depth - 2):
+            if i == 0:
+
+
+        self.output_layer = nn.Linear(n_units, output_dim)
+
+
+
+        # Define the layers
+        self.dense1 = nn.Linear(input_dim, n_units)
+        self.dense2 = nn.Linear(n_units, n_units)
+        self.d_dense3 = DiscontinuityDense(n_units, d_units, activation = self.activation)
+        self.d_dense4 = DiscontinuityDense(d_units, d_units, activation = self.activation)
+        self.dense5 = nn.Linear(d_units, n_units)
+
+
+        self.out = nn.Linear(n_units, output_dim)  # Linear activation for the output layer
+
+
+    def embed_forward(self, x_cont, x_cat):
+        assert self.embeds is not None, ""
+
+    def forward(self, x):
+        assert self.embeds is None, "for training with embedding use .embed_forward"
+
+        return x
+
+
