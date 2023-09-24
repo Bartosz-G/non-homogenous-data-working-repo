804a815932cb774d3be240f1b9592235dfb5c5fe
bart-sagemaker <bartex.gw@gmail.com>
2023-09-24 22:24:02 +0000

    fixed embedding dimensions

--- models/MlpNetwork.py
+++ models/MlpNetwork.py
@@ -205,6 +205,7 @@ class MLP(nn.Module):
             if x_cat.shape != (0,):
                 x_cat = x_cat.long()
                 x_cat = self.embeds(x_cat)
+                x_cat = x_cat.squeeze(1)
             x = torch.cat([x_cont, x_cat], dim=1)
         elif x_cat is not None or x_cont.shape == (0,):
             x = torch.cat([x_cont, x_cat], dim=1)
